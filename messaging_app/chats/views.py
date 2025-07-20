2fe0321a2c4b07747cd68ea682a14421e727311c
ahmedabdelhady <ahmedabdelhadye@gmail.com>
2025-07-20 04:29:04 +0300

    Create serializers

--- messaging_app/chats/views.py
+++ messaging_app/chats/views.py
@@ -1,19 +1,49 @@
-# views.py
-from rest_framework import generics
-from .models import Conversation
-from .serializers import ConversationSerializer
-from .serializers import MessageCreateSerializer
+from rest_framework import viewsets, status
+from rest_framework.response import Response
+from .models import Conversation, Message, User
+from .serializers import ConversationSerializer, MessageSerializer
+from rest_framework.decorators import action
+from django.shortcuts import get_object_or_404
 
 
-class ConversationDetailView(generics.RetrieveAPIView):
-    queryset = Conversation.objects.prefetch_related("participants", "messages__sender")
+class ConversationViewSet(viewsets.ModelViewSet):
+    queryset = Conversation.objects.all()
     serializer_class = ConversationSerializer
-    lookup_field = "conversation_id"
+
+    def create(self, request, *args, **kwargs):
+        participant_ids = request.data.get("participants", [])
+        if not participant_ids or len(participant_ids) < 2:
+            return Response(
+                {"error": "A conversation needs at least 2 participants."}, status=400
+            )
+
+        conversation = Conversation.objects.create()
+        participants = User.objects.filter(user_id__in=participant_ids)
+        conversation.participants.set(participants)
+        conversation.save()
+
+        serializer = self.get_serializer(conversation)
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
 
 
-class MessageCreateView(generics.CreateAPIView):
-    serializer_class = MessageCreateSerializer
+class MessageViewSet(viewsets.ModelViewSet):
+    queryset = Message.objects.all()
+    serializer_class = MessageSerializer
 
-    def perform_create(self, serializer):
-        # Sender is automatically set in serializer
-        serializer.save()
+    def create(self, request, *args, **kwargs):
+        conversation_id = request.data.get("conversation_id")
+        sender_id = request.data.get("sender_id")
+        message_body = request.data.get("message_body")
+
+        if not conversation_id or not sender_id or not message_body:
+            return Response({"error": "Missing fields."}, status=400)
+
+        conversation = get_object_or_404(Conversation, conversation_id=conversation_id)
+        sender = get_object_or_404(User, user_id=sender_id)
+
+        message = Message.objects.create(
+            conversation=conversation, sender=sender, message_body=message_body
+        )
+
+        serializer = self.get_serializer(message)
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
