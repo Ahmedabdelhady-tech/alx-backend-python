9ee053266e5437890b0adf8b37a29aee1bd19561
ahmedabdelhady <ahmedabdelhadye@gmail.com>
2025-07-20 03:07:18 +0300

    messaging_app

--- messaging_app/chats/models.py
+++ messaging_app/chats/models.py
@@ -1,3 +1,50 @@
+import uuid
 from django.db import models
+from django.contrib.auth.models import AbstractUser
+from django.utils.translation import gettext_lazy as _
 
-# Create your models here.
+
+class UserRole(models.TextChoices):
+    GUEST = "guest", _("Guest")
+    HOST = "host", _("Host")
+    ADMIN = "admin", _("Admin")
+
+
+class User(AbstractUser):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    email = models.EmailField(unique=True)
+    phone_number = models.CharField(max_length=15, blank=True, null=True)
+    role = models.CharField(
+        max_length=10, choices=UserRole.choices, default=UserRole.GUEST
+    )
+    create_by = models.DateField(auto_now_add=True)
+
+    REQUIRED_FIELDS = ["email", "first_name", "last_name"]
+    USERNAME_FIELD = "username"
+
+    def __str__(self):
+        return f"{self.first_name} {self.last_name} ({self.email})"
+
+
+class Conversation(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    participants = models.ManyToManyField(User, related_name="conversations")
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"Conversation {self.id} with {self.participants.count()} participants"
+
+
+class Message(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    sender = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name="sent_messages"
+    )
+    conversation = models.ForeignKey(
+        Conversation, on_delete=models.CASCADE, related_name="messages"
+    )
+    Message_body = models.TextField()
+    sent_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"From {self.sender.username} in {self.conversation.id}: {self.Message_body[:20]}..."
