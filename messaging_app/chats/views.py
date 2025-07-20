29973a800a769ef1033bdc81e965fce0b02fe12f
ahmedabdelhady <ahmedabdelhadye@gmail.com>
2025-07-20 18:28:28 +0300

    fix urls and filters import

--- messaging_app/chats/views.py
+++ messaging_app/chats/views.py
@@ -3,6 +3,7 @@ from rest_framework.response import Response
 from django.shortcuts import get_object_or_404
 from .models import Conversation, Message, User
 from .serializers import ConversationSerializer, MessageSerializer
+from rest_framework import filters
 
 
 class ConversationViewSet(viewsets.ModelViewSet):
