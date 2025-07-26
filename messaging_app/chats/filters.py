6a89962c1bb65c51018c379900dd50dca708eb3e
ahmedabdelhady <ahmedabdelhadye@gmail.com>
2025-07-27 01:15:46 +0300

    task 2

--- messaging_app/chats/filters.py
+++ messaging_app/chats/filters.py
@@ -6,13 +6,16 @@ User = get_user_model()
 
 
 class MessageFilter(django_filters.FilterSet):
-
-    sender = django_filters.ModelChoiceFilter(queryset=User.objects.all())
+    sender = django_filters.CharFilter(
+        field_name="sender__username", lookup_expr="icontains"
+    )
+    receiver = django_filters.CharFilter(
+        field_name="receiver__username", lookup_expr="icontains"
+    )
     created_at = django_filters.DateTimeFromToRangeFilter(
         field_name="created_at", label="Date Range"
     )
 
     class Meta:
         model = Message
-        fields = ["sender", "created_at"]
-        order_by = ["-created_at"]
+        fields = ["sender", "receiver", "created_at"]
--- messaging_app/chats/pagination.py
+++ messaging_app/chats/pagination.py
@@ -7,4 +7,3 @@ class MessagePagination(PageNumberPagination):
     page_size = 20  # Default number of messages per page
     page_size_query_param = 'page_size'  # Allow clients to set the page size
     max_page_size = 100  # Maximum allowed page size
-    last_page_strings = ('last',)  # String to indicate the last page
