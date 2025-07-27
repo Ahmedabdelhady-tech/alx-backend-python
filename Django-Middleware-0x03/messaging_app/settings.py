6c810743667731d8f66f05e04a344a7f5c077e98
ahmedabdelhady <ahmedabdelhadye@gmail.com>
2025-07-27 22:07:15 +0300

    task 1

--- Django-Middleware-0x03/messaging_app/settings.py
+++ Django-Middleware-0x03/messaging_app/settings.py
@@ -77,6 +77,7 @@ MIDDLEWARE = [
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
+    "chats.middleware.RequestLoggingMiddleware",
 ]
 
 ROOT_URLCONF = "messaging_app.urls"
@@ -169,4 +170,3 @@ SIMPLE_JWT = {
     "SLIDING_TOKEN_REFRESH_LIFETIME_LATE_USER": timedelta(days=1),
     "SLIDING_TOKEN_LIFETIME_LATE_USER": timedelta(days=30),
 }
-
